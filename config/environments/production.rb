83ae7c622ed98eafc7d6775506b074c39c7016ed
Marcio M. Ribeiro <marcio.mr@gmail.com>
2010-12-14 14:10:49 -0200

    Mysql gem in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,8 +12,6 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
-config.gem 'mysql'
-
 # Use a different cache store in production
 # config.cache_store = :mem_cache_store
 
